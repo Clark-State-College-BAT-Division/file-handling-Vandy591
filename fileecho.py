9da4d35c4b091902effae093ede892de73db4a47
Rockwell_591 <elaina.rockwell@gisaoh.org>
2026-03-22 21:21:59 -0400

    Update fileecho.py

--- fileecho.py
+++ fileecho.py
@@ -1,2 +1,4 @@
-#Tead in the echo.txt file and display it in the terminal using print()
-#This is a guided practice. Either follow the video or your instructor in class.
+file = open("echo.txt")
+for line in file:
+    print(line)
+file.close()
