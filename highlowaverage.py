cbf3e87b13cf3966ddb7a4be2db80e8417eea9c9
Rockwell_591 <elaina.rockwell@gisaoh.org>
2026-03-22 22:20:28 -0400

    Update highlowaverage.py

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,24 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file: 15
+
+file = open("numbers.txt")
+count = 0
+total = 0
+highest = 0
+lowest = 999999
+for line in file:
+    number = int(line)
+    count += 1
+    total += number
+    if number > highest:
+        highest = number
+    if number < lowest:
+        lowest = number
+average = total / count
+print("count:", count)
+print("total:", total)
+print("average:", average)
+print("highest:", highest)
+print("lowest:", lowest)
+file.close()
